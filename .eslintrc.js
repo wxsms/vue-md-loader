7bbabeaf96d1538b9f4873520f122a3398ed65c8
guokairui <guokairui1@wps.cn>
2021-10-12 14:22:05 +0800

    fix: lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,4 @@
 // http://eslint.org/docs/user-guide/configuring
-
-const IS_PRODUCTION = process.env.NODE_ENV === 'production'
-
-function allowInDevelopment() {
-  return IS_PRODUCTION ? 2 : 1
-}
-
 module.exports = {
   root: true,
   env: {
@@ -22,9 +15,5 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
-    'vue/no-v-html': 0,
-    // allow during development
-    'no-debugger': allowInDevelopment(),
-    'no-unused-expressions': allowInDevelopment(),
   },
 }
